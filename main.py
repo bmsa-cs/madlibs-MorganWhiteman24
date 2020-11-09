912fd54d9e1758f8a21e9882460539eb1f4942c4
Morgan Whiteman <mwhiteman24@biomedscienceacademy.org>
2020-11-09 17:56:10 +0000

    I have finished my MadLibs code.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author: Morgan Whiteman
+Period/Core: 2
 """
+print("Let's play Silly Sentences! ")
+holiday= input("Enter a Holiday. ")
+holiday2= input("Enter a second Holiday. ")
+food1= input("Enter a food. ")
+food2= input("Enter another food. ")
+verb1= input("Enter an action verb. ")
+noun1= input("Enter a noun. ")
+verb2= input("Enter a verb ending in ing. ")
+food3= input("Enter a fruit or vegetable. ")
+
+print(""+ holiday + " is a very exciting time of year for many people! ")
+print(""+ holiday + " in my opinon, is much better than "+ holiday2+".")
+print(""+ food1 + " is staple cusine of "+ holiday +". Another popular food is " + food2 + ".")
+print("My favorite thing to do at " + holiday +" time is to " + verb1+ " at the \n" + noun1 +" near my house.") 
+print(f"There are many ways to celebrate \n{holiday} and not every person does it the same way.")
+print(f"Some familes may celebrate {holiday} by {verb2} {food3}." )
+
+
+
+
